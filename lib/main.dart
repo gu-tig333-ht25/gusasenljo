1e13630afc3216995a4841ae79ce585263047f0d
gusasenljo <206040981+gusasenljo@users.noreply.github.com>
2025-09-18 18:06:10 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 import 'home_screen.dart'; // Importerar vår huvudskärm
 
 void main() {
   runApp(const MyApp()); // Startar appen
-=======
-import 'home_screen.dart';
-
-void main() {
-  runApp(const MyApp()); //kör appen
->>>>>>> Stashed changes
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-<<<<<<< Updated upstream
       home: HomeScreen(), // Appen startar på HomeScreen
-=======
-      home: HomeScreen(), //startar homescreen
->>>>>>> Stashed changes
     );
   }
 }
